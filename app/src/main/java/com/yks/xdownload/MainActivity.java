d60071ee9a5fb21f306fbc9e23c69d5e8e43576e
Yuki-r <282107958@qq.com>
2021-01-08 22:13:28 +0800

    第一次提交

---
+++ app/src/main/java/com/yks/xdownload/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yks.xdownload;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
